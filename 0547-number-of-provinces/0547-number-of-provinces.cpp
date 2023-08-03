396005bf09459f117af9de975dc926f2ed891ede
Sparsh <100155097+SPARSH1608@users.noreply.github.com>
2023-08-03 17:32:19 +0530

    Time: 21 ms (91.61%), Space: 17.1 MB (5.6%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,26 +1,52 @@
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        int provinces = 0;
-        vector<bool> visited(n, false);
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                provinces++;
-                dfs(isConnected, visited, i);
+void bfs(int node, vector<int>adj[],vector<int>& vis){
+    queue<int>q;
+    q.push(node);
+    vis[node]=1;
+    while(!q.empty()){
+        int front=q.front();
+        q.pop();
+        for(int it :adj[front]){
+            if(!vis[it]){
+                q.push(it);
+                vis[it]=1;
             }
         }
-
-        return provinces;
     }
-
-    void dfs(vector<vector<int>>& isConnected, vector<bool>& visited, int node) {
-        visited[node] = true;
-        for (int neighbor = 0; neighbor < isConnected.size(); neighbor++) {
-            if (isConnected[node][neighbor] == 1 && !visited[neighbor]) {
-                dfs(isConnected, visited, neighbor);
-            }
+}
+void dfs(int node, vector<int>adj[], vector<int>&vis){
+    vis[node]=1;
+    for(auto it :adj[node]){
+        if(!vis[it]){
+            dfs(it,adj,vis);
         }
+    }
+}
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        int m=isConnected[0].size();
+   vector<int>adj[isConnected.size()]; //adjacency list banalo
+   for(int i=0;i<n;i++){
+       for(int j=0;j<m;j++){
+           if(isConnected[i][j]==1 and i!=j){
+               adj[i].push_back(j);
+               adj[j].push_back(i);
+           }
+       }
+   }
+vector<int>vis(n,0);
+   //0 based indexing
+   int count=0;
+   for(int i=0;i<n;i++){
+       if(!vis[i]){
+           bfs(i,adj,vis);
+        // dfs(i,adj,vis);
+           count++;
+       }
+
+   }
+   return count;
     }
 };
