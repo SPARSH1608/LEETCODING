ce1f7cfff27196af5169a57ad2dc3a6c03132736
Sparsh <100155097+SPARSH1608@users.noreply.github.com>
2023-08-05 20:40:18 +0530

    Time: 16 ms (75.53%), Space: 16.1 MB (56.82%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    vector<TreeNode*> helper(int start , int end){
+        //base case
+        if(start>end){
+            vector<TreeNode*>base;
+            base.push_back(NULL);
+            return base;
+        }
+
+        //recursive case
+        vector<TreeNode*>ans;
+        for(int i=start;i<=end;i++){
+            vector<TreeNode*>left=helper(start,i-1);
+            vector<TreeNode*>right=helper(i+1,end);
+            for(TreeNode* l :left){
+                for(TreeNode* r:right){
+                    TreeNode* root=new TreeNode(i);
+                    root->left=l;
+                    root->right=r;
+                    ans.push_back(root);
+                }
+            }
+        }
+        return ans;
+    }
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        return helper(1,n);
+    }
+
+};
