6fd6246a15b106d2fe3d2556bafddd34fa2328e6
Sparsh <100155097+SPARSH1608@users.noreply.github.com>
2023-08-10 08:54:25 +0530

    Time: 28 ms (42.75%), Space: 19 MB (5.09%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& gr) {
-        int n = gr.size();
-        vector<int> colour(n, 0);
-
-        for(int node = 0; node < n; node++){
-            if(colour[node] != 0) continue;
-
-            queue<int> q;
-            q.push(node);
-            colour[node] = 1;
-
-            while(!q.empty()){
-                int cur = q.front();
-                q.pop();
-
-                for(int ne : gr[cur]){
-                    if(colour[ne] == 0){
-                        colour[ne] = -colour[cur];
-                        q.push(ne);
-                    }else if(colour[ne] != -colour[cur]){
-                        return false;
-                    }
-                }
-            }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+    
+        vector<int>adj[n];
+        for(int i=0;i<n;i++){
+          for(int j:graph[i]){
+              adj[i].push_back(j);
+              adj[j].push_back(i);
+          }
         }
+        bool ans=true;
+      vector<int>color(n,-1);
 
-        return true;
+      for(int i=0;i<n;i++){
+          if(color[i]==-1){
+               queue<int>q;
+      q.push(i);
+      color[i]=0;
+      while(!q.empty()){
+          int node=q.front();
+          q.pop();
+          for(int it:adj[node]){
+              if(color[it]==-1){
+                  color[it]=!color[node];
+                  q.push(it);
+              }else if(color[it]==color[node]){
+                        ans= false;
+                        break;
+              }
+          }
+      }
+
+          }
+      }
+
+      return ans;
+     
+
     }
 };
