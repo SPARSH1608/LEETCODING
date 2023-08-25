0cd7aa85844ea90f08f1a5ae6b7871cd2d28891f
Sparsh <100155097+SPARSH1608@users.noreply.github.com>
2023-08-25 09:25:33 +0530

    Time: 6 ms (73.74%), Space: 18.9 MB (33.49%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,14 @@
+// Recursive solution (DFS)
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        // base case
+        if(root == NULL)
+            return 0;
+
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+
+        return max(left, right) + 1;
+    }
+};
