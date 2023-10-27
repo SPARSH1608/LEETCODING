5a87aa8bb35fb80902d3b05b76a7825cd1c7a0a0
Sparsh <100155097+SPARSH1608@users.noreply.github.com>
2023-10-27 23:26:07 +0530

    Time: 29 ms (63.61%), Space: 10.7 MB (61.98%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,62 +1,48 @@
-class Solution
-{
+class Solution {
 public:
-    string longestPalindrome(string s)
-    {
-        // https://youtu.be/YVZttWzvyw8
-        string t;
-        t.push_back('#');
-        for (int i = 0; i < s.size(); i++)
-        {
-            t.push_back(s[i]);
-            t.push_back('#');
-        }
+    std::string longestPalindrome(std::string s) {
+        // Check if the input string is empty, return an empty string if so
+        if (s.empty())
+            return "";
 
-        swap(s, t);
-        int n = s.size();
-        vector<int> p(n);
+        // Initialize variables to store the indices of the longest palindrome found
+        std::vector<int> longestPalindromeIndices = {0, 0};
 
-        // Manacher's Algorithm
-        int l = 0, r = -1;
-        for (int i = 0; i < n; i++)
-        {
-            int k;
-            if (i > r)
-                k = 1;
-            else
-                k = min(p[i], r - l);
+        for (int i = 0; i < s.length(); ++i) {
+            // Find the indices of the longest palindrome centered at character i
+            std::vector<int> currentIndices = expandAroundCenter(s, i, i);
 
-            // for checking for palindrome
-            while (i - k >= 0 && i + k < n && s[i - k] == s[i + k])
-                k++;
+            // Compare the length of the current palindrome with the longest found so far
+            if (currentIndices[1] - currentIndices[0] > longestPalindromeIndices[1] - longestPalindromeIndices[0]) {
+                longestPalindromeIndices = currentIndices;
+            }
 
-            p[i] = k;
+            // Check if there is a possibility of an even-length palindrome centered at
+            // character i and i+1
+            if (i + 1 < s.length() && s[i] == s[i + 1]) {
+                // Find the indices of the longest even-length palindrome centered at characters
+                // i and i+1
+                std::vector<int> evenIndices = expandAroundCenter(s, i, i + 1);
 
-            // expansion of palindrome if it is more than the previous boundary
-            if (i + k > r)
-            {
-                // new left and right boundary
-                l = i - k;
-                r = i + k;
+                // Compare the length of the even-length palindrome with the longest found so far
+                if (evenIndices[1] - evenIndices[0] > longestPalindromeIndices[1] - longestPalindromeIndices[0]) {
+                    longestPalindromeIndices = evenIndices;
+                }
             }
         }
 
-        // for getting the ans
-        int mx = 0, idx = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (p[i] > mx)
-            {
-                mx = p[i];
-                idx = i;
-            }
-        }
+        // Extract and return the longest palindrome substring using the indices
+        return s.substr(longestPalindromeIndices[0], longestPalindromeIndices[1] - longestPalindromeIndices[0] + 1);
+    }
 
-        string cur = s.substr(idx - mx + 1, 2 * p[idx] - 1);
-        string ans;
-        for (auto i : cur)
-            if (i != '#')
-                ans.push_back(i);
-        return ans;
+private:
+    // Helper function to find and return the indices of the longest palindrome
+    // extended from s[i..j] (inclusive) by expanding around the center
+    std::vector<int> expandAroundCenter(const std::string &s, int i, int j) {
+        while (i >= 0 && j < s.length() && s[i] == s[j]) {
+            --i;
+            ++j;
+        }
+        return {i + 1, j - 1};
     }
 };
