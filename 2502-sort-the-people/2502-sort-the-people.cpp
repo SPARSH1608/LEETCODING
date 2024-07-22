d164617a44e393202021d26c6671f42ea343ea59
Sparsh <sparshgoelk@gmail.com>
2024-07-22 23:44:33 +0530

    Time: 32 ms (45.8%), Space: 27.6 MB (30.21%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        map<int,string,greater<int>>m;
-        for(int i=0;i<heights.size();i++){
+        map<int, string,greater<int>>m;
+        for(int i=0;i<names.size();i++){
             m[heights[i]]=names[i];
         }
-        vector<string>v;
-       for(pair p:m){
-           v.push_back(p.second);
-       }
-       return v;
+        vector<string>ans;
+
+        for(auto it:m){
+            ans.push_back(it.second);
+        }
+        return ans;
     }
 };
