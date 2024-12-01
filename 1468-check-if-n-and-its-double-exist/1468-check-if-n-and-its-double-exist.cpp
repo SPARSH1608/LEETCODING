7329ad22b367cd30e4640c4054f166b528433fd7
Sparsh <sparshgoelk@gmail.com>
2024-12-01 21:55:47 +0530

    Time: 1 ms (33.79%), Space: 13.9 MB (15.78%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,13 +1,27 @@
+// class Solution {
+// public:
+//     bool checkIfExist(vector<int>& arr) {
+//        for( int i=0;i<arr.size();i++){
+//         for(int j=0;j<arr.size();j++){
+//             if(i!=j && arr[i]==2*arr[j]){
+//                 return true;
+//             }
+//         }
+//        } 
+//        return false;
+//     }
+// };
+
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-       for( int i=0;i<arr.size();i++){
-        for(int j=0;j<arr.size();j++){
-            if(i!=j && arr[i]==2*arr[j]){
-                return true;
-            }
+       set<int>s;
+       for(int i=0;i<arr.size();i++){
+        if(s.count(arr[i]*2) || (s.count(arr[i]/2) && arr[i]%2==0)){
+            return true;
         }
-       } 
+        s.insert(arr[i]);
+       }
        return false;
     }
 };
