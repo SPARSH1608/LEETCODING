a42fc2d2c3d890a5df3eab8dd080f1bf79722c0e
Sparsh <sparshgoelk@gmail.com>
2025-01-03 00:58:17 +0530

    Time: 0 ms (100%), Space: 77 MB (83.25%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,57 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-     void insertail(ListNode* &head,ListNode* &tail,int digit){
-        ListNode* temp=new ListNode(digit);
-        if(head==NULL){
-            head=temp;
-            tail=temp;
-        return;
-        }else{
-            tail->next=temp;
-            tail=temp; 
-        }
+ListNode* add(ListNode* head1,ListNode*head2){
+    ListNode* t1=head1;
+    ListNode* t2=head2;
+    ListNode* dummy=new ListNode(-1);
+    ListNode* curr=dummy;
+int carry=0;
+    while(t1!=NULL || t2!=NULL){
+        int sum=carry;
+        if(t1) sum+=t1->val;
+        if(t2) sum+=t2->val;
+        ListNode* newNode=new ListNode(sum%10);
+        carry=sum/10;
+        curr->next=newNode;
+        curr=curr->next;
+        if(t1)t1=t1->next;
+        if(t2)t2=t2->next;
     }
-    ListNode* adds(ListNode* l1,ListNode* l2){
-        int c=0;
-        ListNode* anshead=NULL;
-        ListNode* anstail=NULL;
-
-        while(l1!=NULL&&l2!=NULL){
-        int sum=c+l1->val+l2->val;
-        int digit=sum%10;
-        insertail(anshead,anstail,digit);
-         c=sum/10;
-         l1=l1->next;
-         l2=l2->next;
-        }
-        while(l1!=NULL){
-            int sum=c+l1->val;
-            int digit=sum%10;
-        insertail(anshead,anstail,digit);
-        c=sum/10;
-        l1=l1->next;
-        
-        }
-        while(l2!=NULL){
-            int sum=c+l2->val;
-            int digit=sum%10;
-        insertail(anshead,anstail,digit);
-        c=sum/10;
-        
-         l2=l2->next;
-        }
-        while(c!=0){
-            int sum=c;
-             int digit=sum%10;
-        insertail(anshead,anstail,digit);
-        c=sum/10;
-        }
-        return anshead;
+    if(carry){
+               ListNode* newNode=new ListNode(carry);
+               curr->next=newNode;
     }
-public:
+    return dummy->next;
+}
+ ListNode* reverseLL1(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        while(curr!=NULL){
+            ListNode *n=curr->next;
+            curr->next=prev;
+            prev=curr;
+          curr=n;
+        }
+        return prev;
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        //rev input linked lis
-        ListNode* ans=adds(l1,l2);
-        return ans;
+        if(l1==NULL) return l2;
+        if(l2==NULL) return l1;
+     
+
+        ListNode*head=add(l1,l2);
+        
+        return head;
     }
 };
