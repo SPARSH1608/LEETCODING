feac602725703778e773cf20e50a738f8299f9f3
Sparsh <sparshgoelk@gmail.com>
2025-02-07 00:53:03 +0530

    Time: 857 ms (8.46%), Space: 99.1 MB (16.3%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int tupleSameProduct(std::vector<int>& nums) {
+       map<int, int> map;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                int product = nums[i] * nums[j];
+                count += (map[product] * 8);
+                map[product]++;
+            }
+        }
+        return count;
+    }
+};
