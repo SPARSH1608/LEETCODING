14362ff0b8e2b34d21e4369ad08e6dea364e7ccd
Sparsh <sparshgoelk@gmail.com>
2025-02-09 03:01:44 +0530

    Time: 206 ms (5.37%), Space: 204.2 MB (6.27%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -0,0 +1,71 @@
+class NumberContainers {
+public:
+ map<int,int> hm;
+    map<int,set<int>> mp;
+
+    
+    
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        
+     if(hm.count(index)!=0){
+         int a = hm[index];
+         
+       
+        set<int>::iterator it;
+         
+         it = mp[a].find(index);
+         mp[a].erase(it);
+       
+         
+        
+         hm[index]= number;
+         
+         if(mp.count(number)!=0 && mp[number].size()!=0){
+             
+             mp[number].insert(index);
+        
+         }
+         else{
+             set<int> st ;
+             st.insert(index);
+             mp[number] = st;
+         }
+         
+     }
+        else{
+            
+            hm[index] = number;
+               if(mp.count(number)!=0){
+             
+             mp[number].insert(index);
+        
+         }
+         else{
+             set<int> st ;
+             st.insert(index);
+             mp[number] = st;
+         }
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    int find(int number) {
+        
+       if(mp.count(number)!=0 && mp[number].size()!=0){
+           
+           return *mp[number].begin();
+       }
+        
+        return -1;
+
+    }
+};
